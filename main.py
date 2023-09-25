3a39f827074ae58b6783db6f14ef175c3d61184d
Chen Duo <frank2152565@gmail.com>
2023-09-26 00:57:37 +0800

    complete

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import dict
 
-mode = input("choose encode or decode morse:")
-words = input("Type word:").upper()
-if mode == "encode":
-    for word in words:
-
-
-else:
+sw = True
+while sw:
+    mode = input("choose encode or decode morse:")
+    words = input("Type word:").upper()
+    output = ''
+    if mode == "decode":
+        for word in words:
+            output += dict.decode[word]
+            output += ' '
+    else:
+        for word in words:
+            output += dict.encode[word]
+            output += ' '
+    print(f"result:{output}")
+    conti = input("type ok to continue:")
+    if conti == 'ok':
+        sw = True
+    else:
+        sw = False
